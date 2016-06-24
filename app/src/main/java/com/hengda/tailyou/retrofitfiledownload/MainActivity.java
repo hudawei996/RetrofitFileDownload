bf483baf991ffd8810c90e6cfb6a1c2f7e040923
Tailyou <1173752145@qq.com>
2016-06-24 10:09:40 +0800

    下载进度可视化

--- app/src/main/java/com/hengda/tailyou/retrofitfiledownload/MainActivity.java
+++ app/src/main/java/com/hengda/tailyou/retrofitfiledownload/MainActivity.java
@@ -16,6 +16,9 @@ import retrofit2.Call;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView txtProgress;
+    HDialogBuilder hDialogBuilder;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,23 +28,10 @@ public class MainActivity extends AppCompatActivity {
         //        http://hengdawb-app.oss-cn-hangzhou.aliyuncs.com/app-debug.apk
         String baseUrl = "http://hengdawb-app.oss-cn-hangzhou.aliyuncs.com/";
         String fileName = "app-debug.apk";
-        String fileStoreDir = Environment.getExternalStorageDirectory()
-                .getAbsolutePath() + File.separator + "M_DEFAULT_DIR";
+        String fileStoreDir = Environment.getExternalStorageDirectory().getAbsolutePath() + File.separator + "M_DEFAULT_DIR";
         String fileStoreName = fileName;
-
-        final TextView txtProgress = (TextView) View.inflate(MainActivity.this, R.layout
-                .layout_hd_dialog_custom_tv, null);
-        final HDialogBuilder hDialogBuilder = new HDialogBuilder(MainActivity.this);
-        hDialogBuilder.setCustomView(txtProgress).title("下载").nBtnText("取消")
-                .nBtnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        hDialogBuilder.dismiss();
-                    }
-                }).show();
-
-        FileApi.getInstance(baseUrl)
-                .loadFileByName(fileName, new FileCallback(fileStoreDir, fileStoreName) {
+        showLoadingDialog();
+        FileApi.getInstance(baseUrl).loadFileByName(fileName, new FileCallback(fileStoreDir, fileStoreName) {
                     @Override
                     public void onSuccess(File file) {
                         super.onSuccess(file);
@@ -50,9 +40,7 @@ public class MainActivity extends AppCompatActivity {
 
                     @Override
                     public void progress(long progress, long total) {
-                        txtProgress.setText(String.format("正在下载：(%s/%s)",
-                                DataManager.getFormatSize(progress),
-                                DataManager.getFormatSize(total)));
+                        updateProgress(progress, total);
                     }
 
                     @Override
@@ -62,6 +50,37 @@ public class MainActivity extends AppCompatActivity {
                     }
 
                 });
+    }
+
+    /**
+     * 更新下载进度
+     *
+     * @param progress
+     * @param total
+     */
+    private void updateProgress(long progress, long total) {
+        txtProgress.setText(String.format("正在下载：(%s/%s)",
+                DataManager.getFormatSize(progress),
+                DataManager.getFormatSize(total)));
+    }
+
+    /**
+     * 显示下载对话框
+     */
+    private void showLoadingDialog() {
+        txtProgress = (TextView) View.inflate(MainActivity.this, R.layout
+                .layout_hd_dialog_custom_tv, null);
+        hDialogBuilder = new HDialogBuilder(MainActivity.this);
+        hDialogBuilder.setCustomView(txtProgress)
+                .title("下载")
+                .nBtnText("取消")
+                .nBtnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        hDialogBuilder.dismiss();
+                    }
+                })
+                .show();
     }
 
 }
